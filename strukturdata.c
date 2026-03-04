2a7a7106d99e5a9305dd56acb3679d9b2008349a
sysFaisal <faisalldot.id@gmail.com>
2026-03-04 20:36:36 +0700

    pisahNode

--- strukturdata.c
+++ strukturdata.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node {
     int data;
@@ -25,6 +26,7 @@ void newNode(Node **Head, int dat){
 }
 
 void cekNode(Node **Head){
+    printf("\n");
     Node *Temp = *Head;
     while (Temp != NULL){
         printf("%d [%p] -> %p\n", 
@@ -43,19 +45,48 @@ void gabungNode(Node **Head, Node **Head1){
     ngab->next = *Head1;
 }
 
-int main (){
-    Node *Head = NULL;
-    Node *Head1 = NULL;
-    for (int i = 0; i < 3; i++){
-        newNode(&Head, i);
+void inputNode(Node **Head){
+    char *list = "123456789";
+    printf("Total Lenght = %lld\n", strlen(list));
+    char temp[2];
+
+    for (int i = 0; i < strlen(list); i++){
+        temp[0] = list[i];
+        temp[1] = '\0';
+        printf("%d ", atoi(temp));
+
+        if (i == strlen(list) - 1 ){
+            printf("\n");
+        }
+        newNode(Head, atoi(temp));
     }
-    for (int i = 0; i < 3; i++){
-        newNode(&Head1, 3-i);
+    
+};
+
+void pisahNode(Node **Head){
+    int target = 10;
+    Node *temp = *Head;
+    Node *temp1 = NULL;
+    while(temp->next != NULL){
+
+        if (temp->data == target){
+            temp1 = temp->next;
+            break;
+
+        } else { temp = temp->next; }
     }
 
-    gabungNode(&Head, &Head1);
-    printf("DONE");
+    printf("%d", temp->data);
+    temp->next =NULL;
+    temp1 = NULL;
+};
+
+int main (){
+    Node *Head = NULL;
+
+    inputNode(&Head);
     cekNode(&Head);
-    getchar();
-    getchar();
+    pisahNode(&Head);
+    cekNode(&Head);
+
 }
